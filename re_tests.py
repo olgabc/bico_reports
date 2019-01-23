ec11ec4110bbcd9333ba303b8e5d64072a8d737d
olgabc <nikonova_olga@bk.ru>
2019-01-23 19:42:10 +0300

    Delete re_tests.py

--- re_tests.py
+++
@@ -1,5 +0,0 @@
-import re
-regex = r"<tr (.+?) \[/P\]+?"
-line = "President <tr Barack Obama [/P] met Microsoft founder <tr Bill Gates [/P], yesterday."
-person = re.findall(regex, line)
-print(person)
